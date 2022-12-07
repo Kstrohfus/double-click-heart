7821f9c6fa9417eacd80649cc92af7215840bfc4
Kstrohfus <houston11@me.com>
2022-12-06 21:37:00 -0700

    Function complete with heart, CSS bug need to find keyframe issue

--- js/script.js
+++ js/script.js
@@ -4,5 +4,25 @@ const timesLoved = document.querySelector("#times")
 
 
 loveMe.addEventListener("dblclick", (e) => {
-    console.log(123)
+    createHeart(e)
 })
+
+const createHeart = (e) => {
+    const heart = document.createElement("i")
+    heart.classList.add("fas")
+    heart.classList.add("fa-heart")
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const LeftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - LeftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+}
