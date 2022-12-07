cbe052b6450603a22b09ee68fc12c8d2784f1b9c
Kstrohfus <houston11@me.com>
2022-12-06 21:09:28 -0700

    Added double click function using click method before going back to dbl

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const loveMe = document.querySelector(".loveMe")
 const timesLoved = document.querySelector("#times")
 
+let clickTime = 0
 
-loveMe.addEventListener("dblclick", (e) => {
-console.log(123)
+loveMe.addEventListener("click", (e) => {
+ if(clickTime === 0) {
+    clickTime = new Date().getTime()
+ } else {
+    if((new Date().getTime() - clickTime) < 800) {
+   } else {
+        clickTime = new Date().getTime()
+    }
+ }
 })
