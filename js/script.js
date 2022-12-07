5c31dee2e09f8089ceb33a7ec20d7758d72981b9
Kstrohfus <houston11@me.com>
2022-12-06 21:11:58 -0700

    Function brought back to dblclick

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 const loveMe = document.querySelector(".loveMe")
 const timesLoved = document.querySelector("#times")
 
-let clickTime = 0
 
-loveMe.addEventListener("click", (e) => {
- if(clickTime === 0) {
-    clickTime = new Date().getTime()
- } else {
-    if((new Date().getTime() - clickTime) < 800) {
-   } else {
-        clickTime = new Date().getTime()
-    }
- }
+
+loveMe.addEventListener("dblclick", (e) => {
+    console.log(123)
 })
